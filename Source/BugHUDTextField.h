ce259d3e88d155d815b638f0de0fe289533cfeef
Michael Dippery <mdippery@gmail.com>
2009-09-13 17:05:17 -0400

    Re-licensed under the GPLv3.
    The Creative Commons license, while good for creative works, is
    imperfect for software, and even the Creative Commons advises that
    it not be used for software. The GPLv3 seems to do the best job of
    capturing the spirit of the old license, and should be suitable for
    new code.

--- Source/BugHUDTextField.h
+++ Source/BugHUDTextField.h
@@ -1,13 +1,18 @@
 /*
- * BugHUDTextField.h
- * Copyright (c) 2007 Michael Dippery <mdippery@bucknell.edu>
+ * Copyright (c) 2006-2009 Michael Dippery <mdippery@gmail.com>
  *
- * All rights reserved. This file is licensed under the Creative Commons
- * Attribution-NonCommercial-ShareAlike license, v2.5. You may use this file so
- * long as you follow the guidelines in the license. You may obtain a copy of the
- * license at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     http://creativecommons.org/licenses/by-nc-sa/2.5/legalcode
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
