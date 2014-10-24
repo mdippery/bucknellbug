25181da162a551df3dd0d7b2f21fa429080e4108
Michael Dippery <michael@monkey-robot.com>
2014-10-23 21:06:12 -0700

    Stub of basic service class for Dark Sky API

---
+++ Source/BBDarkSkyService.m
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2006-2011 Michael Dippery <michael@monkey-robot.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import "BBDarkSkyService.h"
+
+
+@implementation BBDarkSkyService
+
++ (NSURL *)defaultURL
+{
+    static NSURL *defaultURL = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSString *url = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"BBDarkSkyURL"];
+        defaultURL = [[NSURL alloc] initWithString:url];
+        NSLog(@"Loaded weather data URL: %@", defaultURL);
+    });
+    return defaultURL;
+}
+
+@end
