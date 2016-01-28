bad7fff95d824d07bb143ff2be57cc17e950e1a5
Michael Dippery <michael@monkey-robot.com>
2016-01-27 16:09:25 -0800

    Update copyright for 2016

--- Source/BBDarkSkyService.m
+++ Source/BBDarkSkyService.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Michael Dippery <michael@monkey-robot.com>
+ * Copyright (c) 2015-2016 Michael Dippery <michael@monkey-robot.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
